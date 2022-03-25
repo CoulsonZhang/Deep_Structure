5da539843571489d6818238af9d4247206b9953b
Coulson Zhang <zheyu3@illinois.edu>
2022-03-25 17:42:54 -0500

    update get_info

--- info_fetch/entry.py
+++ info_fetch/entry.py
@@ -1,3 +1,6 @@
+import utilities as u
+import common_references as c
+
 #1. read file (list of author txt file)
 #1.5 function auto correct name
 #2. Function to get info structure of all authors
@@ -5,3 +8,30 @@
 
 #4 generate matrix1
 #5 generate matrix2 .....
+
+#1
+def get_names(filename):
+    with open(filename, 'r') as file:
+        data = file.read()
+        names = data.split('\n')
+    for i in range(len(names)):
+        names[i] = auto_correct(names[i])
+
+    return names
+#1.5
+def auto_correct(name):
+    #auto check name in mathscinet website
+    pass
+#2
+def fetch_author_info(name):
+    pass
+
+#3
+def fetch_paper_info(name):
+    u.find_citation()
+    pass
+
+
+
+
+
