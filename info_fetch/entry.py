0088a83cc3f87bee7c162fc86d1691cbd3783b01
Coulson Zhang <zheyu3@illinois.edu>
2022-03-25 17:21:59 -0500

    update entry file

--- info_fetch/entry.py
+++ info_fetch/entry.py
@@ -1,4 +1,7 @@
 #1. read file (list of author txt file)
+#1.5 function auto correct name
 #2. Function to get info structure of all authors
 #3. Function to get info structure of all papers
 
+#4 generate matrix1
+#5 generate matrix2 .....
