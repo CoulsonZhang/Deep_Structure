cbb09a3c4f27505da2c13153f0372078f5b291c8
Sujeet B <12060698+sujeet-bhalerao@users.noreply.github.com>
2023-08-07 11:44:38 -0500

    update verify_citation_data.py

--- info_fetch/verify_citation_data.py
+++ info_fetch/verify_citation_data.py
@@ -3,19 +3,30 @@ import os
 
 path = os.getcwd()
 
-with open(path + '/data/citations/KatzSheldonH_citations.json', 'r') as f:
-    data = json.load(f)
+import os
+import json
 
-name = 'Katz, Sheldon H.'
+directory = './data/citations/'
 
-values = list(data.values())
-keys = list(data.keys())
 
-total_length = 0
-for key in data:
-    if isinstance(data[key], list): 
-        list_length = len(data[key])
-        print('The number of citations for paper {} is: {}'.format(key, list_length))
-        total_length += list_length  
+for filename in os.listdir(directory):
+    if filename.endswith('.json'):  
+        with open(os.path.join(directory, filename), 'r') as f:
+            data = json.load(f)
 
-print('The total number of citations for professor {} is: {}'.format(name, total_length)) 
+        name = filename.split('_')[0]
+
+        values = list(data.values())
+        keys = list(data.keys())
+
+        total_length = 0
+        for key in data:
+            if isinstance(data[key], list): 
+                list_length = len(data[key])
+                #print('The number of citations for paper {} is: {}'.format(key, list_length))
+                total_length += list_length  
+
+        print('The total number of citations for professor {} is: {}'.format(name, total_length)) 
+
+
+
